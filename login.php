895dc139ba10b79751b9a9ba603fdfa55b1f53c0
MartinIden2085 <102947117+MartinIden2085@users.noreply.github.com>
2022-11-10 17:16:09 +0300

    10.11.2022

--- login.php
+++ login.php
@@ -35,14 +35,14 @@ if($_SERVER['REQUEST_METHOD']=='POST' && empty($errors)){
     $stmt->execute(['email'=>$user['email']]);
 
     //если количество записей не равно нулю, то пользователь с такой почтой уже есть, добавят нельзя. формируем ошибку
-    if($stmt->rowCount() !=0){
+    if($stmt->rowCount() ==0){
         $errors['email']='Пользователь с этим email уже зарегистрирован';
     } else{
         $dbUser=$stmt->fetch();
         if(password_verify($user["password"],$dbUser["password"])){
         $_SESSION["user_id"]=$dbUser["id"];
         $_SESSION["user_name"]=$dbUser["name"];
-        header("Location: login.php");
+        header("Location: index.php");
         } else{
             $errors["password"]="Неверный пароль";
         }
